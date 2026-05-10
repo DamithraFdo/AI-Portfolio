ae0781e0847eea60a8882b889b740a51dba0cb95
DamithraFdo <damithrafdo@gmail.com>
2026-05-11 00:33:06 +0530

    Fix Vercel config

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,11 @@
-import path from "path";
-
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production'
-
 const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
 
-  output: 'export',
-
   images: {
     unoptimized: true,
-  },
-
-  ...(isProd && {
-    basePath: '/AI-Portfolio',
-    assetPrefix: '/AI-Portfolio/',
-  }),
-
-  turbopack: {
-    root: path.join(process.cwd()),
   },
 }
 
